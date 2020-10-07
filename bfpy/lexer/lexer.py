7137917374cd24ebde8af113289e660bbefa058f
ameyuuno <ameyuuno@gmail.com>
2020-10-08 00:02:40 +0300

    implement lexer

--- bfpy/lexer/lexer.py
+++ bfpy/lexer/lexer.py
@@ -1,11 +1,13 @@
-__all__ = ["Lexer", "LexerImpl"]
+__all__ = ["Lexer", "LexerImpl", "LexerError"]
 
 import abc
 import typing as t
 
-from overrides import overrides
+from bfpy.lexer.token import Token, TokenType, Lexeme
 
-from bfpy.lexer.token import Token
+
+class LexerError(Exception):
+    pass
 
 
 class Lexer(abc.ABC):
@@ -14,6 +16,28 @@ class Lexer(abc.ABC):
 
 
 class LexerImpl(Lexer):
-    @overrides
+    __TOKENS: t.Mapping[t.Text, Token] = {
+        "<": Token(Lexeme("<"), TokenType.LSHIFT),
+        ">": Token(Lexeme(">"), TokenType.RSHIFT),
+        "+": Token(Lexeme("+"), TokenType.INC),
+        "-": Token(Lexeme("-"), TokenType.DEC),
+        ",": Token(Lexeme(","), TokenType.RB),
+        ".": Token(Lexeme("."), TokenType.WB),
+        "[": Token(Lexeme("["), TokenType.OLOOP),
+        "]": Token(Lexeme("]"), TokenType.CLOOP),
+        " ": Token(Lexeme(" "), TokenType.WHITESPACE),
+        "\t": Token(Lexeme("\t"), TokenType.WHITESPACE),
+        "\n": Token(Lexeme("\n"), TokenType.WHITESPACE),
+    }
+
+    __TOKEN_EOF = Token(Lexeme(""), TokenType.EOF)
+
     def tokenize(self, text: t.Text) -> t.Sequence[Token]:
-        raise NotImplementedError
+        return [*(self.__parse_token(char) for char in text), self.__TOKEN_EOF]
+
+    def __parse_token(self, char: t.Text) -> Token:
+        try:
+            return self.__TOKENS[char]
+
+        except KeyError as exc:
+            raise LexerError(f"Unknown token (lexeme={char})") from exc
