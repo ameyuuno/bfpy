5e61e6c22c48db54948445df4fa51172bb4692d1
ameyuuno <ameyuuno@gmail.com>
2020-10-15 23:06:07 +0300

    add tests for intermediate language operation

---
+++ bfpy/il/operation.py
@@ -0,0 +1,47 @@
+__all__ = ["Operation", "Addition", "Subtraction", "LeftShift", "RightShift", "ReadByte", "WriteByte",
+           "CompositeOperation"]
+
+import dataclasses as dc
+import typing as t
+
+
+@dc.dataclass(frozen=True)
+class Operation:
+    def __post_init__(self) -> None:
+        if self.__class__ == Operation:
+            raise TypeError("Can not create an instance of class `Operation`")
+
+
+@dc.dataclass(frozen=True)
+class Addition(Operation):
+    delta: int
+
+
+@dc.dataclass(frozen=True)
+class Subtraction(Operation):
+    delta: int
+
+
+@dc.dataclass(frozen=True)
+class LeftShift(Operation):
+    delta: int
+
+
+@dc.dataclass(frozen=True)
+class RightShift(Operation):
+    delta: int
+
+
+@dc.dataclass(frozen=True)
+class ReadByte(Operation):
+    pass
+
+
+@dc.dataclass(frozen=True)
+class WriteByte(Operation):
+    pass
+
+
+@dc.dataclass(frozen=True)
+class CompositeOperation(Operation):
+    instructions: t.Sequence[Operation]
---
+++ tests/il/test_operation.py
@@ -0,0 +1,11 @@
+import pytest
+
+from bfpy.il.operation import Operation
+
+
+class TestOperation:
+    def test_can_not_create_instance_operation(self):
+        with pytest.raises(TypeError) as exc_info:
+            Operation()
+
+        assert "Can not create an instance of class `Operation`" == str(exc_info.value)
