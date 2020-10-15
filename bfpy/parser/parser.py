6f74002d1de234886f3da34cdd9dec94f4453c3e
ameyuuno <ameyuuno@gmail.com>
2020-10-15 22:10:38 +0300

    implement parser

--- bfpy/parser/parser.py
+++ bfpy/parser/parser.py
@@ -1,8 +1,8 @@
 import abc
 import typing as t
 
-from bfpy.lexer.token import Token
-from bfpy.parser.ast import AstNode
+from bfpy.lexer.token import Token, TokenType
+from bfpy.parser.ast import AstNode, ListNode, LeafNode
 
 
 class SyntacticError(Exception):
@@ -20,4 +20,29 @@ class Parser(metaclass=abc.ABCMeta):
 
 class ParserImpl(Parser):
     def parse(self, tokens: t.Sequence[Token]) -> AstNode:
-        raise NotImplementedError
+        root_nodes: t.MutableSequence[AstNode] = []
+
+        nodes: t.MutableSequence[AstNode] = root_nodes
+        bodies: t.MutableSequence[t.MutableSequence[AstNode]] = []
+
+        for token in tokens:
+            if token.type_ is TokenType.OLOOP:
+                bodies.append(nodes)
+                nodes = []
+
+            elif token.type_ is TokenType.CLOOP:
+                try:
+                    prev_nodes = bodies.pop()
+                except IndexError as exc:
+                    raise SyntacticError(f"Unexpected token (token={token})") from exc
+
+                prev_nodes.append(ListNode(nodes))
+                nodes = prev_nodes
+
+            else:
+                nodes.append(LeafNode(token))
+
+        if len(bodies) > 0:
+            raise SyntacticError(f"Missing token `[` (token_type={TokenType.OLOOP})")
+
+        return ListNode(root_nodes)
