c661543c614336e416293a12c265e8baad7543a7
ameyuuno <ameyuuno@gmail.com>
2020-10-18 14:12:06 +0300

    create test for interpreter
    with optimized (in bf only) hello world

---
+++ bfpy/core/interpreter/interpreter.py
@@ -0,0 +1,17 @@
+import abc
+
+from bfpy.core.il.operation import Operation
+from bfpy.core.interpreter.tape import Tape
+from bfpy.core.io.stream import ByteInputStream, ByteOutputStream
+
+
+class Interpreter(metaclass=abc.ABCMeta):
+    @abc.abstractmethod
+    def evaluate(self, tape: Tape, byte_input_stream: ByteInputStream, byte_output_stream: ByteOutputStream,
+                 operation: Operation) -> None: ...
+
+
+class InterpreterImpl(Interpreter):
+    def evaluate(self, tape: Tape, byte_input_stream: ByteInputStream, byte_output_stream: ByteOutputStream,
+                 operation: Operation) -> None:
+        raise NotImplementedError
---
+++ tests/core/interpreter/conftest.py
@@ -0,0 +1,39 @@
+import io
+import typing as t
+
+import pytest
+from _pytest.fixtures import SubRequest
+
+from bfpy.core.interpreter.interpreter import Interpreter
+from bfpy.core.interpreter.tape import Tape, FiniteTape
+from bfpy.core.io.stream import ByteInputStream, BytesBidirectionalStreamOverTextIo
+
+
+@pytest.fixture
+def tape() -> Tape:
+    return FiniteTape()
+
+
+@pytest.fixture
+def input_bytes(request: SubRequest) -> io.BytesIO:
+    return t.cast(io.BytesIO, request.param)
+
+
+@pytest.fixture
+def input_stream(input_bytes: io.BytesIO) -> ByteInputStream:
+    return BytesBidirectionalStreamOverTextIo(io.TextIOWrapper(input_bytes))  # type: ignore
+
+
+@pytest.fixture
+def output_bytes(request: SubRequest) -> io.BytesIO:
+    return t.cast(io.BytesIO, request.param)
+
+
+@pytest.fixture
+def output_stream(output_bytes: io.BytesIO) -> ByteInputStream:
+    return BytesBidirectionalStreamOverTextIo(io.TextIOWrapper(output_bytes))  # type: ignore
+
+
+@pytest.fixture(params=[])
+def interpreter(request: SubRequest) -> Interpreter:
+    return t.cast(Interpreter, request.param)
