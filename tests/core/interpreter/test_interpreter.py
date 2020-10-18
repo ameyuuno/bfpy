da678ec756e83ae1020e1421659b4eb2e224bf77
ameyuuno <ameyuuno@gmail.com>
2020-10-18 16:42:36 +0300

    add test cases for interpreter

--- tests/core/interpreter/test_interpreter.py
+++ tests/core/interpreter/test_interpreter.py
@@ -3,15 +3,84 @@ import typing as t
 
 import pytest
 
-from bfpy.core.il.operation import (Operation, Program, Addition, RightShift, LeftShift, Subtraction,
-                                    WriteByte, Loop)
+from bfpy.core.il.operation import Program, Addition, Subtraction, LeftShift, RightShift, WriteByte, Loop, ReadByte
 from bfpy.core.interpreter.interpreter import Interpreter
 from bfpy.core.io.stream import ByteInputStream, ByteOutputStream
 
 
 class TestInterpreter:
-    @pytest.mark.parametrize("input_bytes, output_bytes", [(io.BytesIO(), io.BytesIO())], indirect=True)
-    @pytest.mark.parametrize("program, expected_output", [
+    @pytest.mark.parametrize("program, input_bytes, output_bytes, expected_output", [
+        pytest.param(
+            Program([]),
+            io.BytesIO(),
+            io.BytesIO(),
+            b"",
+            id="Empty program"
+        ),
+        pytest.param(
+            Program([
+                Addition(1),
+                Addition(1),
+                Addition(1),
+                Addition(1),
+                Addition(1),
+                Addition(1),
+                Addition(1),
+                Addition(1),
+                Addition(1),
+                Addition(1),
+                Addition(1),
+                Addition(1),
+                Addition(1),
+                Addition(1),
+                Addition(1),
+                Addition(1),
+                Addition(1),
+                Addition(1),
+                Addition(1),
+                Addition(1),
+                Addition(1),
+                Addition(1),
+                Addition(1),
+                Addition(1),
+                Addition(1),
+                Addition(1),
+                Addition(1),
+                Addition(1),
+                Addition(1),
+                Addition(1),
+                Addition(1),
+                Addition(1),
+                Addition(1),
+                Addition(1),
+                Addition(1),
+                WriteByte(),
+            ]),
+            io.BytesIO(),
+            io.BytesIO(),
+            b"#",
+            id="+++++++++++++++++++++++++++++++++++."
+        ),
+        pytest.param(
+            Program([
+                Addition(35),
+                WriteByte(),
+            ]),
+            io.BytesIO(),
+            io.BytesIO(),
+            b"#",
+            id="+(35)."
+        ),
+        pytest.param(
+            Program([
+                Subtraction(130),
+                WriteByte(),
+            ]),
+            io.BytesIO(),
+            io.BytesIO(),
+            b"~",
+            id="-(130)."
+        ),
         pytest.param(
             Program([
                 Addition(1),
@@ -126,15 +195,26 @@ class TestInterpreter:
                 RightShift(1),
                 WriteByte(),
             ]),
+            io.BytesIO(),
+            io.BytesIO(),
             b"Hello World!\n",
             id="++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.------"
                "--.>+.>."
         ),
-    ])
-    def test_evaluate_without_read_byte_operations(self, interpreter: Interpreter,
-                                                   input_stream: ByteInputStream, output_stream: ByteOutputStream,
-                                                   input_bytes: io.BytesIO, output_bytes: io.BytesIO,
-                                                   program: Program, expected_output: t.ByteString) -> None:
+        pytest.param(
+            Program([
+                ReadByte(),
+                WriteByte(),
+            ]),
+            io.BytesIO(b"1"),
+            io.BytesIO(),
+            b"1",
+            id=",."
+        ),
+    ], indirect=["input_bytes", "output_bytes"])
+    def test_evaluate(self, interpreter: Interpreter, program: Program, expected_output: t.ByteString,
+                      input_stream: ByteInputStream, output_stream: ByteOutputStream,
+                      input_bytes: io.BytesIO, output_bytes: io.BytesIO) -> None:
         interpreter.evaluate(input_stream, output_stream, program)
 
         assert output_bytes.getvalue() == expected_output
