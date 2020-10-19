5fe84bdff920146278b0423db868895682f42920
ameyuuno <ameyuuno@gmail.com>
2020-10-19 19:32:47 +0300

    wrap machine part errors with machine error

--- bfpy/core/machine/machine.py
+++ bfpy/core/machine/machine.py
@@ -5,6 +5,7 @@ import typing as t
 
 from bfpy.core.il.translator import Translator
 from bfpy.core.interpreter.interpreter import Interpreter
+from bfpy.core.interpreter.tape import TapeError
 from bfpy.core.io.stream import ByteInputStream, ByteOutputStream
 from bfpy.core.lexer.lexer import Lexer, LexicalError
 from bfpy.core.parser.parser import Parser, SyntacticError
@@ -31,11 +32,21 @@ class MachineImpl(Machine):
         self.__byte_output_stream = byte_output_stream
 
     def execute(self, code: t.Text) -> None:
+        try:
+            self.__execute(code)
+
+        except LexicalError as error:
+            raise MachineError(f"Lexical error: {error}") from error
+
+        except SyntacticError as error:
+            raise MachineError(f"Syntax error: {error}") from error
+
+        except TapeError as error:
+            raise MachineError(f"Evaluation error: {error}") from error
+
+    def __execute(self, code: t.Text) -> None:
         tokens = self.__lexer.tokenize(code)
         ast = self.__parser.parse(tokens)
         program = self.__translator.translate(ast)
 
-        self.__execute(program)
-
-    def __execute(self, program: Program) -> None:
         self.__interpreter.evaluate(self.__byte_input_stream, self.__byte_output_stream, program)
