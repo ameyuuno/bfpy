065be094e10c7d3a1d0ac8176314344ef1c03a1b
ameyuuno <ameyuuno@gmail.com>
2020-10-19 18:41:56 +0300

    create machine implementation

--- bfpy/core/machine/machine.py
+++ bfpy/core/machine/machine.py
@@ -1,9 +1,25 @@
-__all__ = ["Machine"]
+__all__ = ["Machine", "MachineImpl"]
 
 import abc
 import typing as t
+
+from bfpy.core.il.translator import Translator
+from bfpy.core.interpreter.interpreter import Interpreter
+from bfpy.core.lexer.lexer import Lexer
+from bfpy.core.parser.parser import Parser
 
 
 class Machine(metaclass=abc.ABCMeta):
     @abc.abstractmethod
     def execute(self, code: t.Text) -> None: ...
+
+
+class MachineImpl(Machine):
+    def __init__(self, lexer: Lexer, parser: Parser, translator: Translator, interpreter: Interpreter) -> None:
+        self.__lexer = lexer
+        self.__parser = parser
+        self.__translator = translator
+        self.__interpreter = interpreter
+
+    def execute(self, code: t.Text) -> None:
+        raise NotImplementedError
--- tests/core/machine/conftest.py
+++ tests/core/machine/conftest.py
@@ -4,7 +4,12 @@ import typing as t
 import pytest
 from _pytest.fixtures import SubRequest
 
-from bfpy.core.machine.machine import Machine
+from bfpy.core.il.translator import TranslatorImpl
+from bfpy.core.interpreter.interpreter import InterpreterImpl
+from bfpy.core.interpreter.tape import FiniteTape
+from bfpy.core.lexer.lexer import LexerImpl
+from bfpy.core.machine.machine import Machine, MachineImpl
+from bfpy.core.parser.parser import ParserImpl
 
 
 @pytest.fixture
@@ -19,4 +24,9 @@ def output_bytes(request: SubRequest) -> io.BytesIO:
 
 @pytest.fixture
 def machine() -> Machine:
-    raise NotImplementedError
+    lexer = LexerImpl()
+    parser = ParserImpl()
+    translator = TranslatorImpl()
+    interpreter = InterpreterImpl(FiniteTape())
+
+    return MachineImpl(lexer, parser, translator, interpreter)
