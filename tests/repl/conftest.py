dfa5d0f07bbb3a16eb4c2893faedd7fdcab928f7
ameyuuno <ameyuuno@gmail.com>
2020-10-21 18:02:23 +0300

    implement repl

--- tests/repl/conftest.py
+++ tests/repl/conftest.py
@@ -4,19 +4,48 @@ import typing as t
 import pytest
 from _pytest.fixtures import SubRequest
 
-from bfpy.repl.repl import Repl
+from bfpy.core.il.translator import TranslatorImpl
+from bfpy.core.interpreter.interpreter import InterpreterImpl
+from bfpy.core.interpreter.tape import FiniteTape
+from bfpy.core.lexer.lexer import LexerImpl
+from bfpy.core.machine.machine import MachineImpl
+from bfpy.core.parser.parser import ParserImpl
+from bfpy.repl.repl import Repl, ReplImpl
 
 
 @pytest.fixture
-def input_bytes(request: SubRequest) -> io.BytesIO:
+def reader_byte_stream(request: SubRequest) -> io.BytesIO:
+    if not hasattr(request, "param"):
+        return io.BytesIO()
+
     return t.cast(io.BytesIO, request.param)
 
 
 @pytest.fixture
-def output_bytes(request: SubRequest) -> io.BytesIO:
+def writer_byte_stream(request: SubRequest) -> io.BytesIO:
+    if not hasattr(request, "param"):
+        return io.BytesIO()
+
     return t.cast(io.BytesIO, request.param)
+
+
+@pytest.fixture
+def reader(reader_byte_stream: io.BytesIO) -> t.TextIO:
+    return io.TextIOWrapper(reader_byte_stream)
+
+
+@pytest.fixture
+def writer(writer_byte_stream: io.BytesIO) -> t.TextIO:
+    return io.TextIOWrapper(writer_byte_stream, write_through=True)
 
 
 @pytest.fixture
 def repl() -> Repl:
-    raise NotImplementedError
+    return ReplImpl(
+        MachineImpl(
+            LexerImpl(),
+            ParserImpl(),
+            TranslatorImpl(),
+            InterpreterImpl(FiniteTape()),
+        ),
+    )
